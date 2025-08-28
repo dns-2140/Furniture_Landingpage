d29d1b619f471d071fd9df56447eb9b9e6efc908
David Sagala <davidsagala2140@gmail.com>
2025-08-28 14:19:18 +0700

    finish footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,62 +41,159 @@ const Footer = () => {
 
       <div class="border-t border-gray-700 my-6 hidden md:block mb-12"></div>
 
-      <div className="flex md:justify-center items-center">
-        {/*Our Products and Top Feature*/}
-        <div className="flex gap-20">
-          <div className="flex flex-col w-1/2">
-            <h3 class="font-raleway text-white font-semibold mb-6">
-              Our Products
-            </h3>
-            <ul class="space-y-4 font-inter font-normal text-sm leading-5 tracking-normal flex flex-col">
-              <li>
-                <a href="#" className="hover:text-white">
-                  The Support Suite
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  The Sales Suite
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Support
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Guide
-                </a>
-              </li>
-            </ul>
+      <div className="flex flex-col md:flex-row md:justify-center items-start gap-16 md:items-start">
+        <div className="group-1">
+          {/*Our Products and Top Feature*/}
+          <div className="flex gap-20">
+            {/*Our Products*/}
+            <div className="flex flex-col ">
+              <h3 class="font-raleway text-white font-semibold mb-6">
+                Our Products
+              </h3>
+              <ul class="space-y-4 font-inter font-normal text-sm leading-5 tracking-normal flex flex-col">
+                <li>
+                  <a href="#" className="hover:text-white">
+                    The Support Suite
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    The Sales Suite
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Support
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Guide
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/*Top Feature*/}
+            <div className="flex flex-col">
+              <h3 class="font-raleway text-white font-semibold mb-6">
+                Top Features
+              </h3>
+              <ul class="space-y-4 flex flex-col font-inter font-normal text-sm leading-5 tracking-normal">
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Ticketing System
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Knowledge Base
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Community Forums
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Help Desk Software
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="flex flex-col w-1/2">
-            <h3 class="font-raleway text-white font-semibold mb-6">
-              Top Features
-            </h3>
-            <ul class="space-y-4 flex flex-col font-inter font-normal text-sm leading-5 tracking-normal">
-              <li>
-                <a href="#" className="hover:text-white">
-                  Ticketing System
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Knowledge Base
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Community Forums
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Help Desk Software
-                </a>
-              </li>
-            </ul>
+        </div>
+        <div className="group-2">
+          {/*Resources and Company*/}
+          <div className="flex gap-20">
+            {/*Resources*/}
+            <div className="flex flex-col">
+              <h3 class="font-raleway text-white font-semibold mb-6">
+                Resources
+              </h3>
+              <ul class="space-y-4 font-inter font-normal text-sm leading-5 tracking-normal flex flex-col">
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Product Support
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Request Demo
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Library
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    People power blog
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/*Company*/}
+            <div className="flex flex-col">
+              <h3 class="font-raleway text-white font-semibold mb-6">
+                Company
+              </h3>
+              <ul class="space-y-4 flex flex-col font-inter font-normal text-sm leading-5 tracking-normal">
+                <li>
+                  <a href="#" className="hover:text-white">
+                    About us
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Press
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Investors
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    Help Desk Software
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="group-3">
+          {/*Favorite things*/}
+          <div className="flex gap-20">
+            <div className="flex flex-col">
+              <h3 class="font-raleway text-white font-semibold mb-6">
+                Favourite Things
+              </h3>
+              <ul class="space-y-4 font-inter font-normal text-sm leading-5 tracking-normal flex flex-col">
+                <li>
+                  <a href="#" className="hover:text-white">
+                    For Enterprise
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    For Startups
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    For Benchmark
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-white">
+                    For Small Businesses
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
